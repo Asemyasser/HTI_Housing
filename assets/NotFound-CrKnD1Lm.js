2e76740ea7a422d722a1ce34c6d6d1f502da31e9
Asemyasser <asemyasser42@gmail.com>
2024-12-26 20:13:06 +0200

    Updates

---
+++ assets/NotFound-CrKnD1Lm.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DGb4r7-Z.js";const t=()=>e.jsxs("div",{className:"text-center",children:[e.jsx("h1",{children:"404 - Page Not Found"}),e.jsx("p",{children:"The page you are looking for does not exist."})]});export{t as default};
