ed3be0533e319325b704fbdbd633f0bd1b71a532
Asemyasser <asemyasser42@gmail.com>
2024-12-30 07:17:58 +0200

    Updates

---
+++ assets/NotFound-D_udaz67.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CJyA49r_.js";const t=()=>e.jsxs("div",{className:"text-center",children:[e.jsx("h1",{children:"404 - Page Not Found"}),e.jsx("p",{children:"The page you are looking for does not exist."})]});export{t as default};
